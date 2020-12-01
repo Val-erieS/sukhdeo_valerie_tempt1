eece02d5e28f79dde7351ea821cd8f5ec5f58f47
Val-erieS <72756972+Val-erieS@users.noreply.github.com>
2020-11-30 19:30:58 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 while True:
     print("temperature: " + input.temperature(TemperatureUnit.FAHRENHEIT))
-    if input.temperature(TemperatureUnit.FAHRENHEIT) > 112:
+    if input.temperature(TemperatureUnit.FAHRENHEIT) > 113:
         light.set_all (light.rgb(225, 0, 0))
     elif input.temperature(TemperatureUnit.FAHRENHEIT) > 94:
         light.set_all (light.rgb(0, 225, 0))
--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 while (true) {
     console.log("temperature: " + input.temperature(TemperatureUnit.Fahrenheit))
-    if (input.temperature(TemperatureUnit.Fahrenheit) > 112) {
+    if (input.temperature(TemperatureUnit.Fahrenheit) > 113) {
         light.setAll(light.rgb(225, 0, 0))
     } else if (input.temperature(TemperatureUnit.Fahrenheit) > 94) {
         light.setAll(light.rgb(0, 225, 0))
